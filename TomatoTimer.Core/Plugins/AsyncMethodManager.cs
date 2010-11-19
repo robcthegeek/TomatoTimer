7e4c710c129545ed8de31de57e708e10a7108692
Rob Cooper (robcthegeek) <robcthegeek.public@gmail.com>
2010-11-19 07:23:27 +0000

    Started Fleshing Out AsyncMethodManager. Added Method Hook w/ Start/Completed Events

---
+++ TomatoTimer.Core/Plugins/AsyncMethodManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TomatoTimer.Core.Plugins
+{
+    public class AsyncMethodManager
+    {
+        public AsyncMethodHook Run(Action action)
+        {
+            if (action == null)
+                throw new ArgumentNullException(
+                    "action", "Action is Required to Run Async");
+
+            var hook = new AsyncMethodHook(action);
+            action();
+            return hook;
+        }
+    }   
+}
